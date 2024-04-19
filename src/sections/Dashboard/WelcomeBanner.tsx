ec01cda17d7076d3ca7993e2897b1ef0234dea27
STEWIEsol <167385144+STEWIEsol@users.noreply.github.com>
2024-04-19 21:13:50 +0300

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -38,6 +38,44 @@ const Buttons = styled.div`
 `;
 
 const Welcome = styled.div`
+  background: linear-gradient(135deg, #141e30, #243b55);
+  border-radius: 10px;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+  filter: drop-shadow(0 4px 3px rgba(0,0,0,.07)) drop-shadow(0 2px 2px rgba(0,0,0,.06));
+
+  & img {
+    animation-duration: 5s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    width: 100px;
+    height: 100px;
+    top: 0;
+    right: 0;
+    &:nth-child(1) {animation-delay: 0s;}
+    &:nth-child(2) {animation-delay: 1s;}
+  }
+
+  & > div {
+    padding: 0px;
+    filter: drop-shadow(0 4px 3px rgba(0,0,0,.07)) drop-shadow(0 2px 2px rgba(0,0,0,.06));
+  }
+
+  @media (min-width: 800px) {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    padding: 0;
+    & > div {
+      padding: 40px;
+    }
+  }
+`;
+const Welcome = styled.div`
   background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
   border-radius: 10px;
   position: relative;
