9472851008401aa9eebe8bd2e625795871afa9fc
STEWIEsol <167385144+STEWIEsol@users.noreply.github.com>
2024-04-20 14:36:41 +0300

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -76,13 +76,19 @@ const Welcome = styled.div`
       padding: 40px;
     }
   }
+
+  h1, p {
+    border: 2px solid black;
+    padding: 10px;
+    font-size: 24px; /* Adjust font size as needed */
+  }
 `;
 
 export function WelcomeBanner() {
   return (
     <Welcome>
       <div>
-        <h1><img src="/logo.svg" alt="Stewie Casino Logo" />Stewie Casino</h1>
+        <h1>Stewie Casino</h1>
         <p>New Listing LICKGOATCOIN.</p>
         <button className="small-button" onClick={() => window.open('https://twitter.com/LickGoatCoin', '_blank')}>
            Twitter <img src="/x.png" alt="X" style={{ width: '0.8em', height: '0.8em', marginLeft: '4px' }} />
